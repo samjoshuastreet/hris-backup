b10eaa30807ecc26c9b3bf507cf5126a3bda8d62
ielmn <62684358+ielmn@users.noreply.github.com>
2024-02-14 15:54:08 +0800

    Initial commit

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -29,4 +29,21 @@
             <span key="t-contacts">Employee</span>
         </a>
     </li>
+    <li>
+        <a href="javascript: void(0);" class="has-arrow waves-effect">
+            <i class="bx bx-calendar-check"></i>
+            <span key="t-multi-level">Attendance</span>
+        </a>
+        <ul class="sub-menu" aria-expanded="true">
+            <li><a href="/attendance" key="t-level-1-1">Check In/Out</a></li>
+            <li>
+                <a href="javascript: void(0);" class="has-arrow" key="t-level-1-2">Attendance Record</a>
+                <ul class="sub-menu" aria-expanded="true">
+                    {{-- @foreach($departments as $key => $department)
+                    <li><a href="{{ route('view_department', ['id' => $department->id]) }}" key="t-level-2-{{ $key }}">{{ $department->department_name }}</a></li>
+                    @endforeach --}}
+                </ul>
+            </li>
+        </ul>
+    </li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -38,3 +39,5 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/employees', [EmployeeController::class, 'index']);
+
+Route::get('/attendance', [AttendanceController::class, 'index']);
